121b6eabb7515d8f8a4603ede993a1f355c0edf4
Fernando <fpano@isyc.com>
2023-07-24 17:32:19 +0200

    Remove plugins

--- messageagent/db/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-$messageproviders = array (
-
-    'individual_message' => array (
-    ),
-
-);
--- particular_messageagent/db/messages.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-$messageproviders = array (
-    'particular_message' => array (
-    ),
-
-);
