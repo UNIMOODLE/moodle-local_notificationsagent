a04dab2a6cc4380e2520e409f2ee28e62105e0f5
Fernando <fpano@isyc.com>
2023-10-25 18:39:17 +0200

    fix issue#27

--- condition/activityopen/classes/observer.php
+++ condition/activityopen/classes/observer.php
@@ -40,7 +40,7 @@ class notificationscondition_activityopen_observer {
 
         $timestart = notificationsagent_condition_activityopen_get_cm_starttime($cmid);
 
-        $pluginname = '_activityopen';
+        $pluginname = 'activityopen';
 
         $conditions = notificationsagent::get_conditions_by_cm($pluginname, $courseid, $cmid);
         $context = context_course::instance($courseid);
