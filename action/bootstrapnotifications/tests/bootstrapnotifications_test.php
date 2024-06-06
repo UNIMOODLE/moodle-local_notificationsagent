832442c8f6960600133e201747fc36f7451381a1
Fernando <fpano@isyc.com>
2024-06-06 18:54:23 +0200

    Test fixed

--- action/bootstrapnotifications/tests/bootstrapnotifications_test.php
+++ action/bootstrapnotifications/tests/bootstrapnotifications_test.php
@@ -110,7 +110,7 @@ class bootstrapnotifications_test extends \advanced_testcase {
         self::$subplugin = new bootstrapnotifications(self::$rule->to_record());
         self::$subplugin->set_id(5);
         self::$coursetest = self::getDataGenerator()->create_course(
-            ['startdate' => self::COURSE_DATESTART, 'enddate' => self::COURSE_DATEEND]
+                ['startdate' => self::COURSE_DATESTART, 'enddate' => self::COURSE_DATEEND]
         );
         self::$coursecontext = \context_course::instance(self::$coursetest->id);
         self::$user = self::getDataGenerator()->create_user();
@@ -136,13 +136,14 @@ class bootstrapnotifications_test extends \advanced_testcase {
         self::$context->set_params($param);
         self::$context->set_rule(self::$rule);
         self::$context->set_userid(self::$user->id);
+        self::$context->set_usertimesfired(1);
         self::$subplugin->set_id(self::CONDITIONID);
         $USER->id = self::$user->id;
         // Test action.
         self::$subplugin->execute_action(self::$context, $param);
 
         $messages = bootstrapmessages::get_records(
-            ['userid' => self::$context->get_userid(), 'courseid' => self::$context->get_courseid()]
+                ['userid' => self::$context->get_userid(), 'courseid' => self::$context->get_courseid()]
         );
 
         $params = self::$context->get_params();
@@ -158,8 +159,8 @@ class bootstrapnotifications_test extends \advanced_testcase {
      */
     public static function dataprovider(): array {
         return [
-            ['{"message":"TEST"}'],
-            ['{"message":"Message content"}'],
+                ['{"message":"TEST"}'],
+                ['{"message":"Message content"}'],
         ];
     }
 
@@ -197,8 +198,8 @@ class bootstrapnotifications_test extends \advanced_testcase {
      */
     public function test_checkcapability() {
         $this->assertSame(
-            has_capability('local/notificationsagent:' . self::$subtype, self::$coursecontext),
-            self::$subplugin->check_capability(self::$coursecontext)
+                has_capability('local/notificationsagent:' . self::$subtype, self::$coursecontext),
+                self::$subplugin->check_capability(self::$coursecontext)
         );
     }
 
@@ -238,10 +239,10 @@ class bootstrapnotifications_test extends \advanced_testcase {
         $courseid = self::$coursetest->id;
         $typeaction = "add";
         $customdata = [
-            'rule' => self::$rule->to_record(),
-            'timesfired' => rule::MINIMUM_EXECUTION,
-            'courseid' => $courseid,
-            'getaction' => $typeaction,
+                'rule' => self::$rule->to_record(),
+                'timesfired' => rule::MINIMUM_EXECUTION,
+                'courseid' => $courseid,
+                'getaction' => $typeaction,
         ];
 
         $form = new editrule_form(new \moodle_url('/'), $customdata);
@@ -265,8 +266,8 @@ class bootstrapnotifications_test extends \advanced_testcase {
     public function test_processmarkups() {
         $UI_MESSAGE = 'test message';
         $expected = str_replace(
-            self::$subplugin->get_elements(), [shorten_text(str_replace('{' . Rule::SEPARATOR . '}', ' ', $UI_MESSAGE))],
-            self::$subplugin->get_title()
+                self::$subplugin->get_elements(), [shorten_text(str_replace('{' . Rule::SEPARATOR . '}', ' ', $UI_MESSAGE))],
+                self::$subplugin->get_title()
         );
         $params[self::$subplugin::UI_MESSAGE] = $UI_MESSAGE;
         $params = json_encode($params);
