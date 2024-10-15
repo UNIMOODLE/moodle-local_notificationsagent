4b3468afed753700a6fcf2c0e97c57438f96a310
Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
2024-10-15 08:05:41 +0000

    Auto-deploy actions.

--- version.php
+++ version.php
@@ -35,7 +35,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_notificationsagent';
-$plugin->release = '0.6.0';
-$plugin->version = 2024061700;
+$plugin->release = '0.6.1';
+$plugin->version = 2024101500;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_BETA;
