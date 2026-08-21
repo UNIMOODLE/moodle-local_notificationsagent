af18cb9f9bb1620382ea19de6ed3080fca4e2a40
Fernando <fpano@isyc.com>
2026-08-21 13:12:21 +0200

    added shortcut in case no conditions are found

--- condition/activitystudentend/classes/observer.php
+++ condition/activitystudentend/classes/observer.php
@@ -58,9 +58,11 @@ class notificationscondition_activitystudentend_observer {
         $userid = $event->userid;
         $timecreated = $event->timecreated;
 
-        activitystudentend::set_activity_access($userid, $courseid, $cmid, $timecreated);
-
         $conditions = notificationsagent::get_conditions_by_cm($pluginname, $courseid, $cmid);
+
+        if (!empty($conditions)) {
+            activitystudentend::set_activity_access($userid, $courseid, $cmid, $timecreated);
+        }
 
         foreach ($conditions as $condition) {
             $conditionid = $condition->id;
